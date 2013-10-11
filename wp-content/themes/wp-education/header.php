1033ceb806c707c4d65b4dad973eb334d9ab987b
limtrack <limtrack@hal.(none)>
2013-10-11 15:12:16 +0200

    [Sprint 38] add favicon

--- wp-content/themes/wp-education/header.php
+++ wp-content/themes/wp-education/header.php
@@ -29,6 +29,8 @@
 	<!-- Starting wp_head -->
 	<?php wp_head(); ?>
 
+	<link rel="shortcut icon" href="<?php echo get_stylesheet_directory_uri(); ?>/favicon.ico" />
+
 </head>
 
 <body <?php body_class(); ?>>
